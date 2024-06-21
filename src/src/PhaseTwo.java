edc04e2d96f8dedbbf732dd11afa70f8edd423cd
DeReals <85774740+DeReals@users.noreply.github.com>
2024-06-20 22:37:56 -0400

    Organized files

---
+++ src/src/PhaseTwo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PhaseTwo {
+	int totalPhaseTwoPoints;
+	
+	int arrow() {
+		int arrowPoints;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("How many arrows?: ");
+		int arrowAmountInput = scan.nextInt();
+		arrowPoints = arrowAmountInput * 10800;
+		System.out.println("|");
+		
+		this.totalPhaseTwoPoints += arrowPoints;
+		
+		return arrowPoints;
+	}
+	
+	
+	public int ToString() {
+		System.out.println("|Phase TWO|");
+		System.out.print("Amount of arrow points: " + arrow() + "\n\n");
+		return this.totalPhaseTwoPoints;
+	}
+}
